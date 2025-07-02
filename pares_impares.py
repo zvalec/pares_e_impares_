e068f58991e2fceaa58e267730b5795820a9fe42
zvalec <va.bruna@duocuc.cl>
2025-07-01 20:21:09 -0400

    Agrego Try except

--- pares_impares.py
+++ pares_impares.py
@@ -5,14 +5,15 @@ while True:
     entrada = input("Ingrese un número: ")
     if entrada.lower() == "salir":
         break
-
-    numero = int(entrada)
-    if numero % 2 == 0:
+    try
+        numero = int(entrada)
+        if numero % 2 == 0:
         pares += 1
         
-    else:
+        else:
         impares += 1
-
+except ValueError:
+         print("Entrada no válida. Escriba un número o 'salir'.")
 
 print("La cantidad de números pares es:", pares, "y la cantidad de números impares es:", impares)
 
